df5bf612e41cba7e6e7f616d118e5721412df6a8
Sumit Kumar Das <shivrajkumardas111@gmail.com>
2026-01-18 17:14:14 +0545

    Working with Model

--- Dj_Beginner/settings.py
+++ Dj_Beginner/settings.py
@@ -2,6 +2,8 @@ import os
 
 from pathlib import Path
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -117,3 +119,5 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')  #for images handle
--- Dj_Beginner/urls.py
+++ Dj_Beginner/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),   # admin should ALWAYS be here::usernmae:Sumit and PD-->@sum11
@@ -10,19 +12,6 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('Chaye/', include('Chaye.urls') ),
 
+    path("__reload__/", include("django_browser_reload.urls")),  # setup for hot reloading
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-
-
-
-
-    path("__reload__/", include("django_browser_reload.urls")),#setup for hot reloading
-
-
-
-
-
-
-
-
-
-]
