3a3cc44c5c009b9a46023d208cb94369845954b8
Sumit Kumar Das <shivrajkumardas111@gmail.com>
2026-01-26 19:55:41 +0545

    Simple Form Validation

--- Chaye/forms.py
+++ Chaye/forms.py
@@ -2,7 +2,8 @@ from django import forms
 from .models import Chaivarity
 
 class ChaiVarityForm(forms.Form):
-    chai_varity=forms.ModelChoiceField(queryset=Chaivarity.objects.all(),label="Select Chai Varity")
+     chai_varity=forms.ModelChoiceField(queryset=Chaivarity.objects.all(),label="Select Chaiii Varity")
+
 
     #ModelChoiceField--> it will automatically made choice acc, to the request made by client
     #object.all()--> sapai objects hru lai ChaiVarity relation bata nikalxa
--- Chaye/views.py
+++ Chaye/views.py
@@ -15,20 +15,28 @@ def chai_details(request ,chai_id):
     chai=get_object_or_404(Chaivarity,pk=chai_id)
     return render(request,'Chaye/chai_detail.html',{'chai':chai})
 
+
 def chai_store_view(request):
-    stores=None  ##stores--> form submit vaisakepaxi frontent ma yo variable pass grne so that we can use it as instance odf obj. WHICH IS PASSED USING: {'stores':stores} in return section
-    if request.method=='POST':
-        form=ChaiVarityForm(request.POST)  #stores post method data  into form varibale
-        if form.is_valid(): ##to know the submitted form is valid or not
-            chai_variety=form.cleaned_data['chai_varity']   ##chai_varity taken from forms.py / cleaned_data---> u want clean data
+    stores = None  ##stores--> form submit vaisakepaxi frontent ma yo variable pass grne so that we can use it as instance odf obj. WHICH IS PASSED USING: {'stores':stores} in return section
 
-            ##working with store db now
-            stores=Store.objects.filter(chai_varieties=chai_variety)
-        else:
-            form=ChaiVarityForm()  ## if user don't  get his required querry, return there the same form at frontent  i.e default form to user
+    form = ChaiVarityForm()  ## default form for GET request
+
+    if request.method == 'POST':
+        form = ChaiVarityForm(request.POST)  # stores post method data into form variable
+
+        if form.is_valid():  ## to know the submitted form is valid or not
+            chai_variety = form.cleaned_data['chai_varity']   ## chai_varity taken from forms.py / cleaned_data---> u want clean data
+
+            ## working with store db now
+            stores = Store.objects.filter(chai_varieties=chai_variety)
 
 
-    return render(request,'Chaye/chai_stores.html',{'stores':stores , 'form':form})
+    return render(
+        request,
+        'Chaye/chai_stores.html',
+        {'stores': stores, 'form': form}
+    )
+
 
 
 
